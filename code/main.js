13a4b35272e07e9e494329986c694597af704b40
Gitthuma <george.githuma@gmail.com>
2021-05-24 12:35:59 +0300

    Got image using the src propery.

--- code/main.js
+++ code/main.js
@@ -3,3 +3,7 @@
 /*Create a constant myImage and set it to new Image method. This allow myImage to acces the image using the src property*/
 
 const myImage = new Image();
+
+/*Get image using the src propery. Use canvas.toDataURL to convert the image into a url string to avoid cross origin resource errors in some browsers when we scan the image with getImageData later.*/
+
+myImage.src = canvas.toDataURL('../images/disney-junior-T-O-T-S.jpg');
