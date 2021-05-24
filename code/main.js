564baae0c1591a2337bcbdd64214d011f46f4288
Gitthuma <george.githuma@gmail.com>
2021-05-24 12:18:42 +0300

    Created a constant myImage and set it to new Image method.

---
+++ code/main.js
@@ -0,0 +1,5 @@
+/*Bring image into the project*/
+
+/*Create a constant myImage and set it to new Image method. This allow myImage to acces the image using the src property*/
+
+const myImage = new Image();
